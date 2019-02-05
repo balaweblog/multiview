e028e237967ff3619d0d478034e769852e469444
Balamurugan Alagumalai <balamurugan.alagumalai@gmail.com>
2019-02-05 15:37:13 +0530

    Update test.cs

--- test.cs
+++ test.cs
@@ -1,79 +1,107 @@
-'use strict';
-var express = require('express');
-var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
-var cors = require('cors');
-var fs = require('fs')
-var morgan = require('morgan')
-var https = require('https');
-var path = require('path')
-var rfs = require('rotating-file-stream')
-const helmet = require('helmet');
-const expressOasGenerator = require('express-oas-generator');
-var compression = require('compression')
-var RateLimit = require('express-rate-limit');
-const logger = require('./config/log')
+import * as express from 'express';
+import * as Session from 'express-session';
+
+import { getcountries, getstates, getcitiesbystateid, getjoblocation, getskillset} from '../controller/utility/utilitycontroller';
+import { createEmployer } from '../controller/employer/employercontroller';
+import { createJob, getjobs, getjobsbyJobId, getjobsbydate, updatejobStatus } from '../controller/job/jobcontroller';
+import { googleredirect, facebookredirect, facebooklogin, googlelogin, twitterlogin, twitterredirect, localsignup, locallogin } from '../controller/auth/authcontroller';
+import { error, success, loginsuccess, verifywebtoken , encryption, decryption } from '../controller/account/accountcontroller';
+import { userProfile, getuserprofilebyEmail, hasuserProfile, getuserProfilebyLastWorkingday, updateuserProfile, getreferencestatus, updatereferencestatus, getuserprofilebyreference, getprofilereferencestatusbyemail } from '../controller/candidate/candidatecontroller';
+import { getNotification, updateNotification, insertNotification } from '../controller/utility/notificationcontroller';
+import { updatejobstatus, applyjob, getappliedjobs, deleteappliedjob, deleteappliedjobbyJobId, getallappliedjobs, hasappliedjobs } from '../controller/job/applyjobcontroller';
+
+var passport = require('passport');
+const SchemaValidator = require('../validation/schemavalidator');
 
 
-import router from './router/router';
-import config from './config/main';
+export default (app) => {
 
-// init express
-const app = express();
+    const apiRoutes = express.Router();
+    const utitlityroutes = express.Router();
+    const coreroutes = express.Router();
+    const accountroutes = express.Router();
+    const authroutes = express.Router();
 
-var limiter = new RateLimit({
-    windowMs: 60*60*1000, // 15 minutes 
-    max: 10000, // limit each IP to 100 requests per windowMs 
-    delayMs: 0 // disable delaying - full speed until the max limit is reached 
-});
+    // Session secret only server side.
+    app.use(Session({
+        secret: 'raysources-secret-19890913007',
+        resave: true,
+        saveUninitialized: true
+    }));
 
-// rate limiter for all requests
-app.use(limiter);
-  
+    // passport and session 
+    app.use(passport.initialize());
+    app.use(passport.session());
 
-// swagger generator /api-docs
-expressOasGenerator.init(app, {});
-
-
-// https options
-var certDirectory = path.join(__dirname, 'cert')
-var httpsOptions = {
-key: fs.readFileSync(path.join(certDirectory, 'key.pem'),'utf8'),
-cert: fs.readFileSync(path.join(certDirectory, 'server.crt'),'utf8')
-};
-
-// enable log
-app.use(morgan('combined', {stream: logger.stream}))
-
-// enable compression to reduce the size of response body
-app.use(compression())
+    //Joi Validation 
+    const validateRequest = SchemaValidator(false);
 
 
-// init mongoose
-mongoose.connect(config.mongodb.database);
+    // common route
+    apiRoutes.use('/utilities',verifywebtoken,utitlityroutes);
+    apiRoutes.use('/core',coreroutes);
+    apiRoutes.use('/account',accountroutes);
+    apiRoutes.use('/auth',authroutes);
 
-// middleware
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.json());
-app.disable('etag');
+    //utility controller routes 
+    // notification
+    utitlityroutes.put('/notification/:id',updateNotification);
+    utitlityroutes.get('/notification/:status',getNotification);
+    utitlityroutes.post('/notification/:emailaddress',insertNotification);
+    //utility
+    utitlityroutes.get('/countries', getcountries);
+    utitlityroutes.get('/states', getstates);
+    utitlityroutes.get('/cities/:id',getcitiesbystateid);
+    utitlityroutes.get('/joblocations',getjoblocation);
+    utitlityroutes.get('/skillset',getskillset);
 
-// cors enable
-app.use(cors())
+    //core controller routes
+    // job
+    coreroutes.post('/job',createJob);
+    coreroutes.get('/job/:title/:emailaddress/:experience/:minsalary/:maxsalary/:location',getjobs);
+    coreroutes.get('/job/:jobid',getjobsbyJobId);
+    coreroutes.get('/jobs/:currentdate',getjobsbydate);
+    coreroutes.put('/job/:jobid',updatejobStatus);
+    // applied job
+    coreroutes.post('/appliedjob',applyjob);
+    coreroutes.get('/appliedjob/:emailaddress',getappliedjobs);
+    coreroutes.delete('/appliedjob/:jobid',deleteappliedjob);
+    coreroutes.delete('/appliedjobs/:jobid',deleteappliedjobbyJobId);
+    coreroutes.get('/appliedjobbystatus/:jobstatus',getallappliedjobs);
+    coreroutes.put('/appliedjob/:emailaddress',updatejobstatus);
+    coreroutes.get('/appliedjobbyemail/:emailaddress',hasappliedjobs);
+    // employer
+    coreroutes.post('/employer',createEmployer);
+    // user profile
+    coreroutes.post('/userprofile',validateRequest ,userProfile);
+    coreroutes.put('/userprofile/:emailaddress',updateuserProfile);
+    coreroutes.put('/userprofilebyreferencestatus/:emailaddress',updatereferencestatus);
+    coreroutes.get('/userprofilebyreferences/:referencestatus',getuserprofilebyreference);
+    coreroutes.get('/userprofilebyreferencestatus/:emailaddress/:referencename/:referencenumber',
+    getreferencestatus);
+    coreroutes.get('/userprofilebyreferencestatusemail/:emailaddress', 
+    getprofilereferencestatusbyemail);
+    coreroutes.get('/userprofilebyemail/:emailaddress',getuserprofilebyEmail);
+    coreroutes.get('/userprofilebylwd/:days',getuserProfilebyLastWorkingday);
+    coreroutes.get('/userprofile/:emailaddress',hasuserProfile);
 
-// secure http headers
-app.use(helmet({frameguard: {action: 'deny'}}))
-app.disable('x-powered-by')
+    //auth routes 
+    authroutes.get('/facebook',facebooklogin);
+    authroutes.get('/facebookoauthcallback',facebookredirect);
+    authroutes.get('/google', googlelogin);
+    authroutes.get('/googleoauthcallback',googleredirect);
+    authroutes.get('/twitter',twitterlogin);
+    authroutes.get('/twitteroauthcallback',twitterredirect);
+    authroutes.post('/localsignup',localsignup);
+    authroutes.post('/locallogin',locallogin);
+    
+    //account routes
+    accountroutes.get('/error',error);
+    accountroutes.get('/success',success);
+    accountroutes.get('/loginsuccess',loginsuccess);
+    accountroutes.get('/encrypt/:message',encryption);
+    accountroutes.get('/decrypt/:message',decryption);
 
-// router
-router(app);
-
-//mongoose.set('debug', true);
-
-// init server
-let server;
-server = https.createServer(httpsOptions, app).listen(config.server.port,function(){
-    console.log(`node server listening on ${config.server.port}`);
-});
-
-// export
-export default server;
+    // append route to app 
+    app.use('/api',apiRoutes);
+}
