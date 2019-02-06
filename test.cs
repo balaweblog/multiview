0da96ceef99e0c69d36b060a4bc6d318fea4bf77
Balamurugan Alagumalai <balamurugan.alagumalai@gmail.com>
2019-02-06 14:29:24 +0530

    Update test.cs

--- test.cs
+++ test.cs
@@ -1,89 +1,127 @@
-const  nodemon = require("gulp-nodemon");
-const ts = require('gulp-typescript');
-const gulp = require('gulp');
-const del = require("del");
-const pm2 = require('pm2');
+import * as mongoose from 'mongoose';
+import { ObjectId } from 'mongodb';
+import { invalidbody, ok, error, invalidrequest, out, notfound } from '../account/accountcontroller';
+import job from '../../models/job';
+import appliedjob from '../../models/appliedjob';
 
 
-    // pull in the project Typescript config
-    const tsProject = ts.createProject('tsconfig.json');
+/* create new job */
+export function createJob(req,res){
+    if (Object.keys(req.body).length === 0) {
+        invalidbody(res);
+        return;
+    }
+    const jobs = new job(req.body);
+    jobs.save().then(e => {ok(res)}).catch(_ => error(res));
+}
 
-     
-    // copy the certificate folders
-    gulp.task('copy certificate',['clean task '] ,function() {
-      gulp.src('./cert/**')
-      .pipe(gulp.dest('build/cert'));
-   });
+/* get all jobs matching the search criteria */
+export function getjobs(req,res) {
+    const title = req.params["title"];
+    const location = req.params['location'];
+    const emailaddress = req.params['emailaddress'];
+    const experience = Number(req.params['experience']);
+    const minsalary = Number(req.params['minsalary']);
+    const maxsalary = Number(req.params['maxsalary']);
+    let jobinformation: string[] =[];
+    
+    if(title === "" || location === "" || emailaddress === "" || experience < 1 || minsalary < 1 || maxsalary < 1 ){
+        invalidrequest(res);
+        return;
+    }
 
-   // create log folder
-   gulp.task('create log folder',['clean task '],function() {
-    gulp.src('*.*',{read:false})
-    .pipe(gulp.dest('build/log'));
-   });
-
-   // delete build folder
-   gulp.task('clean task ', function(){
-      return del('build/**', {force:true});
-   });
-
-    // task to be run when the watcher detects changes
-    gulp.task('build',['clean task ','copy certificate','create log folder'], () => {
-      const tsResult = tsProject.src()
-      .pipe(tsProject());
-      return tsResult.js.pipe(gulp.dest('build'));
+    // if any job is already applied
+    appliedjob.find({emailaddress:emailaddress},(_, appliedjobsdetails)=>{
+        appliedjobsdetails.forEach(function(_, doc) {
+            jobinformation.push(appliedjobsdetails[doc]["jobid"]);
     });
 
-    // gulp run in prod mode. 
-    gulp.task('deploy', function () {
-        nodemon({ script: 'build/server.js' })
-          .on('restart', function () {
-            console.log('restarted!')
-          })
+    job.find({   
+        $or: [
+            { $and: [{ minexperience: {$lte:experience}, maxexperience: {$gte:experience}}]},
+            { $and: [{ minexperience: {$lte:experience-1}, maxexperience: {$gte:experience-1}}]},
+            { $and: [{ minexperience: {$lte:experience+1}, maxexperience: {$gte:experience+1}}]}
+        ],
+        $and: [
+            { $and: [{maxsalary: {$gte: minsalary-1, $lte: maxsalary+1}}]},
+        ],
+        location: {
+            $regex: location.split(',').join('|'),
+            $options: "i"
+        },
+        skillset: {
+            $regex: title.split(',').join('|'),
+            $options: "i"
+        },
+        _id: { "$nin": jobinformation}}).then(item => 
+            {
+                if (item.length <1) {
+                    notfound(res);
+                } else {
+                    out(res,item)
+                }
+            }).catch(_ => error(res));
     });
-    // pm2 production stop 
-    gulp.task('prodstart', function () {
-      pm2.connect(true, function () {
-          pm2.start({
-              name: 'hotcakeapi',
-              script: 'build/server.js',
-              env: {
+}
+/* get jobs by job Id */
+export function getjobsbyJobId(req,res ){
+    const jobid = req.params["jobid"];
+    const jobidarr = jobid.split(',')
+    for (let i =0; i< jobidarr.length; i++){
+        if (!ObjectId.isValid(jobidarr[i])) {
+            invalidrequest(res);
+            return
+        }
+    }
+    job.find({_id: {$in: jobidarr.map(function(o){ return mongoose.Types.ObjectId(o); })}}).then(item => 
+        {
+            if (item.length<1) {
+                notfound(res);
+            } else {
+                out(res,item)
+            }
+        }).catch(_ => error(res));
+}
+/* get jobs by date */
+export function getjobsbydate(req,res,next) {
 
-              }
-          }, function () {
-              console.log('pm2 hotcakeapi started');
-              pm2.streamLogs('all', 0);
-          });
-      });
-  });
-  // pm2 production stop 
-  gulp.task('prodstop', function () {
-        pm2.connect(true, function () {
-            pm2.stop("hotcakeapi", function() {
-              console.log('pm2 hotcakeapi stopped');
-              pm2.streamLogs('all', 0);
-            });
-        });
-    });
-  // pm2 production restart 
-  gulp.task('prodrestart', function () {
-      pm2.connect(true, function () {
-          pm2.restart("hotcakeapi", function() {
-            console.log('pm2 hotcakeapi restarted');
-            pm2.streamLogs('all', 0);
-          });
-      });
-  });
-  // pm2 production delete 
-  gulp.task('proddelete', function () {
-    pm2.connect(true, function () {
-        pm2.delete("hotcakeapi", function() {
-          console.log('pm2 hotcakeapi deleted');
-          pm2.streamLogs('all', 0);
-        });
-    });
-  });
-  
-    // default task  for watcher
-    gulp.task('default', ['watch']); 
+    if(Date.parse(req.params['currentdate'])<1) {
+        invalidrequest(res);
+        return;
+    }
+    const currentdate = new Date(req.params['currentdate']);
+    const currentdate1 = new Date(req.params['currentdate']);
+    var start = currentdate;
+    start.setHours(0,0,0,0);
+    var end = currentdate1;
+    end.setHours(23,59,59,999);
+    var query = {status:"Active",enddate: {$gte: start, $lte: end}};
 
-export {};
+    job.find(query).then(item => 
+        {
+            if (item.length < 1) {
+                notfound(res);
+            } else {
+                out(res,item)
+            }
+        }).catch(_ => error(res));
+}
+/* update job status */
+export function updatejobStatus(req,res) {
+    const id = req.params.jobid;
+
+    if (!ObjectId.isValid(id)) {
+        invalidrequest(res);
+        return;
+    }
+
+    job.findOneAndUpdate({_id:id}, {status:"InActive"}, {upsert:true}).then(
+        item => {
+            if (item ==null) {
+                notfound(res);
+            } else {
+                ok(res);
+            }
+        }).catch(_ => error(res));
+
+}
