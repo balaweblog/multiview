823a2bbb9ab212c59f889feca9d772990f1cfa6a
Balamurugan Alagumalai <balamurugan.alagumalai@gmail.com>
2019-02-06 16:03:26 +0530

    Update test.cs

--- test.cs
+++ test.cs
@@ -1,18 +1,11 @@
-export function notfound(res) {
-    res.status(404).json({response: "error", message:"no record found"});
-}
-export function ok(res) {
-    res.status(201).json({response: "success", message:"record inserted/updated."});
-}
-export function error(res) {
-    res.status(500).json({response: "error", message:"server error"})
-}
-export function invalidrequest(res) {
-    res.status(400).json({response: "error", message:"invalid http request"})
-}
-export function invalidbody(res) {
-    res.status(400).json({response: "error", message:"invalid message body"})
-}
-export function out(res,output) {
-    res.status(200).json(output);
+  userprofile.findOneAndUpdate(query, update, {upsert:true}).then(
+        item => {
+            if( item ==null) {
+                notfound(res);
+            } else {
+                insertnotificationtable(email,"You Profile Deleted");
+                ok(res);
+            }
+        }).catch(_ => error(res));
+
 }
